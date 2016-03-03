e0040eb32e6e107893f70fedcb854868a265736b
Alignak <nick.fitton96@gmail.com>
2016-03-03 22:29:53 +0000

    Update 1
    + States
    + Buttons
    + Switch
    + Info LED's
    + Ticking systems
    + Off when switch is LOW
    + State change on button push

--- STEM_0_1/STEM_0_1.ino
+++ STEM_0_1/STEM_0_1.ino
@@ -1,36 +1,106 @@
 #include <Adafruit_NeoPixel.h>
-#define PIN         11
-#define NUMPIXELS   300
+#define neoPin       10
+#define numPixels    60
 
 #ifdef __AVR__
 #include <avr/power.h>
 #endif
 
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+int buttonPins[3] = {7, 8, 9};
+int switchPin = 6;
+int ledPins[4] = {2, 3, 4, 5};
+int state = 0;
+double neoTick = 0;
+int counter = 0;
+int pState = 0;
+int tickLength = 500;
 
-int delayval = 1iii00;
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(numPixels, neoPin, NEO_GRB + NEO_KHZ800);
+
+int delayval = 100;
 //STEM Fest Neopixel Design
 
 void setup() {
+  Serial.begin(9600);
 #if defined (__AVR_ATtiny85__)
   if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
 #endif
   pixels.begin();
+  for (int i = 2; i <= 5; i++) {
+    pinMode(i, OUTPUT);
+  }
+  for (int i = 6; i <= 9; i++) {
+    pinMode(i, INPUT);
+  }
 }
 
 void loop() {
-  initialisePixels();
-  for (int i = 1; i < NUMPIXELS; i++) {
-    pixels.setPixelColor(i, pixels.Color(0, 150, 0));
-    pixels.show();
-    delay(delayval);
-  }
-}
+  if (digitalRead(6) == HIGH) { // If switch is ON
 
-void initialisePixels() {
-  for (int i = 1; i < NUMPIXELS; i++) {
-    pixels.setPixelColor(i, pixels.Color(200,0,0));
+    for (int i = 7; i <= 9; i++) { // For each button
+      //      Serial.print("t");
+      if (digitalRead(i) == HIGH) { // If button is pressed
+        state = i - 7; // Switch states depending on the button pressed
+      }
+    }
+    //    Serial.println();
+    if (pState != state) {
+      for (int i = 0; i < numPixels; i++) { // For each NeoPixel
+        //        Serial.print("p");
+        //        pixels.setPixelColor(i, pixels.Color(0, 0, 0)); // Turn the NeoPixel off
+      }
+      //      Serial.println();
+      pixels.show();
+    }
+    digitalWrite(2, HIGH);  // Set the running LED on
+    for (int i = 3; i <= 5; i++) { // For each state led
+      //      Serial.print("l");
+      digitalWrite(i, LOW); // Turn off
+    }
+    //    Serial.println();
+    digitalWrite(state + 3, HIGH); // Turn on the state LED depending on the current state
+    if (neoTick + tickLength < millis()) { // If it's been 500 milliseconds since the last change in neoPixels
+      neoTick = millis(); // Reset the timer
+      Serial.println(millis());
+      counter++; // Step up the counter
+      if (counter >= 10) { // If the counter has exceeded the controlled area
+        counter = 0; // Reset the counter to 0
+      }
+      //      Serial.println(counter);
+    }
+
+    switch (state) { // Depending on the state
+      case 0://   Standby mode
+        for (int i = 0; i < numPixels; i++) { // For each neoPixel
+          //          Serial.print("s");
+          if ((i + counter) % 10 < 5) { // For every 5 set
+            pixels.setPixelColor(i, pixels.Color(200, 0, 0)); // Set the pixel color to red
+          } else {
+            pixels.setPixelColor(i, pixels.Color(200, 200, 200)); // Set the pixel color to white
+            //first five pixels red
+            //second five pixels while
+          }
+        }
+        //        Serial.println();
+        break;
+      case 1: // Attention attraction
+
+        break;
+      case 2: // During a talk
+
+        break;
+    }
+
+  } else { // If the switch is off
+    for (int i = 2; i <= 5; i++) { // For each LED
+      digitalWrite(i, LOW); // Turn the LED off
+    }
+    for (int i = 0; i < numPixels; i++) { // For each NeoPixel
+      //      Serial.print("o");
+      pixels.setPixelColor(i, pixels.Color(0, 0, 0)); // Turn the NeoPixel off
+    }
   }
-  pixels.show();
+  pixels.show(); // Update the NeoPixels
+  pState = state;
 }
 
